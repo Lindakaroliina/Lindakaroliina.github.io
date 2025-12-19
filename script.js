5606716ee2151d1c858d4792e73a5e779778ee70
Lindakaroliina <lindakaroliinaoksanen@outlook.com>
2025-12-19 12:18:21 +0200

    Improved UI

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Tämä on menu näkymä kun leveys pienenee
 function toggleMenu(){
 const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
@@ -5,4 +7,49 @@ const menu = document.querySelector(".menu-links");
     icon.classList.toggle("open");
 
 }
+
+// Vaihda tähän oma salasana
+
+const PASSWORD = "Linda2025";
+
+// Funktio joka tarkistaa salasanan
+
+function checkPw() {
+    const pw = document.getElementById("pw").value;
+
+    if (pw===PASSWORD) {
+        //Tallennetaan  tieto, että ei häitä ulos (sessionStorage)
+        sessionStorage.setItem("portfolio_auth", "1");
+
+        // Näytetään sisältö ja piilotetaan login
+        document.getElementById("login").style.display = "none";
+        document.getElementById("content").style.display = "block";
+    }
+
+    else {
+        document.getElementById("err").innerText = "Väärä salasana";
+    }
+    }
     
+    //Kuunnellaan buttonia kun klikataan
+document.getElementById("openBtn").addEventListener("click", checkPw);
+
+// Kuunnellaan Enter näppäintä
+    document.getElementById("pw").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault(); //Estää lomakkeen lähetyksen
+            checkPw(); // kutsuu samaa funktiota kuin button
+        }
+    }
+);
+
+if (sessionStorage.getItem("portfolio_auth") === "1") {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("content").style.display = "block";
+}
+
+// Valinnainen uloskirjautumisfunktio
+function logout() {
+    sessionStorage.removeItem("portfolio_auth");
+    location.reload(); //päivittää sivun
+}
